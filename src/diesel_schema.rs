8810b63ee672e52c0f8f3efabac63eeb084e5587
krauzerkrip <krauzerkrip@gmail.com>
2026-03-15 21:47:24 +0200

    fix: uncomment diesel tables

--- src/diesel_schema.rs
+++ src/diesel_schema.rs
@@ -48,82 +48,82 @@ diesel::table! {
     }
 }
 
-// code below made by gemini but I think it should be generated automatically
+//code below made by gemini but I think it should be generated automatically
 
-// diesel::table! {
-//     use diesel::sql_types::*;
-//     use pgvector::sql_types::Vector;
+diesel::table! {
+    use diesel::sql_types::*;
+    use pgvector::sql_types::Vector;
 
-//     youtube_channels (id) {
-//         id -> Text,
-//         display_name -> Text,
-//         profile_image_url -> Text,
-//         channel_title -> Nullable<Text>,
-//         date_added -> Timestamptz,
-//         date_updated -> Nullable<Timestamptz>,
-//     }
-// }
+    youtube_channels (id) {
+        id -> Text,
+        display_name -> Text,
+        profile_image_url -> Text,
+        channel_title -> Nullable<Text>,
+        date_added -> Timestamptz,
+        date_updated -> Nullable<Timestamptz>,
+    }
+}
 
-// diesel::table! {
-//     use diesel::sql_types::*;
-//     use pgvector::sql_types::Vector;
+diesel::table! {
+    use diesel::sql_types::*;
+    use pgvector::sql_types::Vector;
 
-//     youtube_comment_threads (id) {
-//         id -> Text,
-//         etag -> Text,
-//         date_added -> Timestamptz,
-//         date_updated -> Timestamptz,
-//     }
-// }
+    youtube_comment_threads (id) {
+        id -> Text,
+        etag -> Text,
+        date_added -> Timestamptz,
+        date_updated -> Timestamptz,
+    }
+}
 
-// diesel::table! {
-//     use diesel::sql_types::*;
-//     use pgvector::sql_types::Vector;
+diesel::table! {
+    use diesel::sql_types::*;
+    use pgvector::sql_types::Vector;
 
-//     youtube_comments (id) {
-//         id -> Text,
-//         parent_id -> Nullable<Text>,
-//         thread_id -> Text,
-//         etag -> Text,
-//         text -> Text,
-//         like_count -> Int4,
-//         author_id -> Text,
-//         video_id -> Text,
-//         date_published -> Timestamptz,
-//         date_added -> Timestamptz,
-//         date_updated -> Nullable<Timestamptz>,
-//         last_checked -> Timestamptz,
-//         deleted -> Bool,
-//     }
-// }
+    youtube_comments (id) {
+        id -> Text,
+        parent_id -> Nullable<Text>,
+        thread_id -> Text,
+        etag -> Text,
+        text -> Text,
+        like_count -> Int4,
+        author_id -> Text,
+        video_id -> Text,
+        date_published -> Timestamptz,
+        date_added -> Timestamptz,
+        date_updated -> Nullable<Timestamptz>,
+        last_checked -> Timestamptz,
+        deleted -> Bool,
+    }
+}
 
-// diesel::table! {
-//     use diesel::sql_types::*;
-//     use pgvector::sql_types::Vector;
+diesel::table! {
+    use diesel::sql_types::*;
+    use pgvector::sql_types::Vector;
 
-//     youtube_videos (id) {
-//         id -> Text,
-//         channel_id -> Nullable<Text>,
-//         title -> Nullable<Text>,
-//         etag -> Nullable<Text>,
-//         view_count -> Nullable<Int8>,
-//         like_count -> Nullable<Int8>,
-//         comment_count -> Nullable<Int8>,
-//         thumbnail -> Jsonb,
-//         date_published -> Nullable<Timestamptz>,
-//         date_added -> Timestamptz,
-//         date_updated -> Nullable<Timestamptz>,
-//         last_checked -> Nullable<Timestamptz>,
-//     }
-// }
+    youtube_videos (id) {
+        id -> Text,
+        channel_id -> Nullable<Text>,
+        title -> Nullable<Text>,
+        etag -> Nullable<Text>,
+        view_count -> Nullable<Int8>,
+        like_count -> Nullable<Int8>,
+        comment_count -> Nullable<Int8>,
+        thumbnail -> Jsonb,
+        date_published -> Nullable<Timestamptz>,
+        date_added -> Timestamptz,
+        date_updated -> Nullable<Timestamptz>,
+        last_checked -> Nullable<Timestamptz>,
+    }
+}
 
-// diesel::joinable!(vestibule_users -> messages (intro_message_id));
+ diesel::joinable!(vestibule_users -> messages (intro_message_id));
 
-// diesel::allow_tables_to_appear_in_same_query!(
-//     messages,
-//     vestibule_users,
-//     youtube_channels,
-//     youtube_comment_threads,
-//     youtube_comments,
-//     youtube_videos,
-// );
+ diesel::allow_tables_to_appear_in_same_query!(
+     messages,
+     vestibule_users,
+    youtube_channels,
+    youtube_comment_threads,
+    youtube_comments,
+    youtube_videos,
+ );
